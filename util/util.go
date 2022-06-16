7751d0107041f1eabcd4292e44020a4a4f075e52
sjqzhang <s_jqzhang@163.com>
2022-06-16 09:40:48 +0800

    add recover

--- util/util.go
+++ util/util.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
+	"runtime/debug"
 )
 
 type logger struct {
 	tag string
 	log *log.Logger
 }
+
 func NewLogger(tag string) *logger {
 	return &logger{tag: tag, log: log.New(os.Stdout, fmt.Sprintf("[%v] ", tag), log.LstdFlags)}
 }
 func (l *logger) SetTag(tag string) {
-	l.tag=tag
+	l.tag = tag
 }
 func (l *logger) Log(msg interface{}) {
 	l.log.Println("\u001B[32m" + fmt.Sprintf("%v", msg) + "\u001B[0m")
@@ -31,3 +34,13 @@ func (l *logger) Panic(msg interface{}) {
 }
 
 var Log *logger = NewLogger("default")
+
+func Recover() {
+	if err := recover(); err != nil {
+		_, file, line, ok := runtime.Caller(3)
+		if ok {
+			errMsg := fmt.Sprintf("[%s] panic file:[%s:%v] recovered:\n%s\n%s", "gmock", file, line, err, string(debug.Stack()))
+			Log.Error(errMsg)
+		}
+	}
+}
